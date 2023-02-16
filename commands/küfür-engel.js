1426773d12727333db8fd853a1c3509e1f04998d
bnewixua <109867535+bnewixua@users.noreply.github.com>
2023-02-16 10:18:23 +0300

    Update küfür-engel.js

--- commands/küfür-engel.js
+++ commands/küfür-engel.js
@@ -18,7 +18,6 @@ module.exports = {
 
     run: async (client, interaction) => {
 
-//BneWixua#1519 Tarafından yapıldı. Daha fazla altyapı için discord.gg/altyapilar
 
         if(wixua.fetch(`kufurEngel_${interaction.guild.id}`)) {
             return interaction.reply({ content: "sistem zaten ayarlı", ephemeral: true })
