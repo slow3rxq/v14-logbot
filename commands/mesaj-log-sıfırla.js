0eb27c743aaead62ee531bb0bfe4c49657104239
bnewixua <109867535+bnewixua@users.noreply.github.com>
2023-06-14 23:18:07 +0300

    Update mesaj-log-sıfırla.js

--- commands/mesaj-log-sıfırla.js
+++ commands/mesaj-log-sıfırla.js
@@ -9,7 +9,6 @@ module.exports = {
 
     run: async (client, interaction) => {
 
-//BneWixua#1519 Tarafından yapıldı. Daha fazla altyapı için discord.gg/npm
 
         const yetki_embed = new EmbedBuilder()
             .setColor("Red")
